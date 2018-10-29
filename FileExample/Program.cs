7fcc5db282a0dbf4afbbf0cb54523922f1c4f2a9
Czeglédi Viktor <czegledi.viktor@gmail.com>
2018-10-29 19:07:44 +0100

    Egyszerű bájt műveletek fájlon

--- FileExample/Program.cs
+++ FileExample/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,35 @@ namespace FileExample
 {
     class Program
     {
+        public Program()
+        {
+            
+        }
+
         static void Main(string[] args)
         {
+            FileMode mode = FileMode.Open;
+            FileStream fs = new FileStream("test.txt", mode, FileAccess.ReadWrite, FileShare.Read);
+            try
+            {
+                while (fs.Position < fs.Length)
+                {
+                    int b = fs.ReadByte();
+                    Console.WriteLine(b + " " + (char) b);
+                }
+
+                fs.WriteByte(33);
+                fs.WriteByte(33);
+                fs.WriteByte(33);
+            }
+            finally
+            {
+                fs.Close();
+                fs = null;
+            }
+            
+
+            Console.ReadKey();
         }
     }
 }
